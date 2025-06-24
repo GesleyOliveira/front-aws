048abc3b9a45074a065245134c87d30a7830ddaf
Gesley de Oliveira Rosa <127444907+GesleyOliveira@users.noreply.github.com>
2025-06-24 20:08:55 -0300

    fix: add App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
 import UserPage from './pages/userPage';
 import Navbar from './components/navbar';
